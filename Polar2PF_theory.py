5dd39c4a1a92ed2681cbcea6a589168a7ed0e17a
maclipp <76782956+maclipp@users.noreply.github.com>
2020-12-30 16:01:11 +0100

    Add files via upload
    Python code containing all the theoretical functions used to fit the data from two-photon excited polarization-dependent microscopy

---
+++ Polar2PF_theory.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 12:29:18 2020
+
+@author: Maciej Lipok
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as integrate
+
+#parameters
+pi=np.pi
+cos=np.cos
+sin=np.sin
+
+#objective collecting parameters calculated basing on the theoretical functions
+K1=2.945
+K2=0.069
+K3=1.016
+
+#dichroic mirror parameters
+delta=0.98845 #for excitation wavelength 810nm, for wavelength 750nm, delta=0.99662. Delta is an ellipticity of used dichroic mirror.
+gamma=0.01 #dichroism of dichroic mirror
+
+#parameters depending on the orientation of molecule and fluorophore, based on Fig.1d
+psi=29*pi/180 # Angle between a dye and a fiber (from 0 to 45)
+dpsi=10*pi/180 #Deviation of PSI angle (between dye and  fiber)
+phi=72*pi/180 #Orientation(angle) of a fiber (from 0 to 180)
+
+
+
+def f(ith,iphi):
+    return np.exp(-((ith-psi)/dpsi)**2)/(2*pi*sin(psi)*dpsi*pi**0.5)
+def uax(ith, iphi,phi):
+    return -sin(phi)*sin(ith)*sin(iphi)+cos(phi)*cos(ith)
+def uay(ith,iphi,phi):
+    return cos(phi)*sin(ith)*sin(iphi)+sin(phi)*cos(ith)
+def uaz(ith,iphi): 
+    return -sin(ith)*cos(iphi)
+def JX(ith,iphi,phi):
+    return K1*uax(ith,iphi,phi)**2+K2*uay(ith,iphi,phi)**2+K3*uaz(ith,iphi)**2
+def JY(ith,iphi,phi):
+    return K2*uax(ith,iphi,phi)**2+K1*uay(ith,iphi,phi)**2+K3*uaz(ith,iphi)**2
+def fxxxxx(phi):
+    return integrate.dblquad(lambda ith, iphi: JX(ith,iphi,phi)*uax(ith,iphi,phi)**4*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fxyyyy(phi):
+    return integrate.dblquad(lambda ith, iphi: JX(ith,iphi,phi)*uay(ith,iphi,phi)**4*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fxxyyy(phi):
+    return integrate.dblquad(lambda ith, iphi: JX(ith,iphi,phi)*uax(ith,iphi,phi)*uay(ith,iphi,phi)**3*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fxxxyy(phi):
+    return integrate.dblquad(lambda ith, iphi: JX(ith,iphi,phi)*uax(ith,iphi,phi)**2*uay(ith,iphi,phi)**2*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fxxxxy(phi):
+    return integrate.dblquad(lambda ith, iphi: JX(ith,iphi,phi)*uay(ith,iphi,phi)*uax(ith,iphi,phi)**3*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fyyyyy(phi):
+    return integrate.dblquad(lambda ith, iphi: JY(ith,iphi,phi)*uay(ith,iphi,phi)**4*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fyxxxx(phi):
+    return integrate.dblquad(lambda ith, iphi: JY(ith,iphi,phi)*uax(ith,iphi,phi)**4*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fyxxyy(phi):
+    return integrate.dblquad(lambda ith, iphi: JY(ith,iphi,phi)*uax(ith,iphi,phi)**2*uay(ith,iphi,phi)**2*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fyxxxy(phi):
+    return integrate.dblquad(lambda ith, iphi: JY(ith,iphi,phi)*uay(ith,iphi,phi)*uax(ith,iphi,phi)**3*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+def fyxyyy(phi):
+    return integrate.dblquad(lambda ith, iphi: JY(ith,iphi,phi)*uax(ith,iphi,phi)*uay(ith,iphi,phi )**3*f(ith,iphi)*sin(ith),0,2*pi,0,pi)[0]
+
+data_size=180 #size of data column
+m=np.arange(1, data_size)
+l=np.arange(1, data_size)
+o=np.arange(1, data_size)
+    
+alph=2*o*pi/data_size
+iphi=l*pi/data_size # angle of orintation of a dipole from z direction
+ith=2*m*pi/data_size # angle of orientation of a dipole in xy plane
+
+Ex4,Ey4,Ex2Ey2,Ex3Ey,ExEy3=np.arange(1.0, data_size),np.arange(1.0, data_size),np.arange(1.0,data_size),np.arange(1.0, data_size),np.arange(1.0, data_size)
+
+for x in range(0, data_size-1):
+        Ex4[x]=integrate.quad(lambda fi: ((1-gamma)*cos(alph[x])*cos(fi))**4,0,2*pi)[0]
+        Ey4[x]=integrate.quad(lambda fi:(sin(alph[x])*cos(fi+delta))**4,0,2*pi)[0]
+        Ex2Ey2[x] =integrate.quad(lambda fi:(((1-gamma)*cos(alph[x])*cos(fi))**2)*((sin(alph[x])*cos(fi+delta))**2),0,2*pi)[0]
+        Ex3Ey[x] =integrate.quad(lambda fi:((((1-gamma)*cos(alph[x])*cos(fi))**3)*sin(alph[x])*cos(fi+delta)),0,2*pi)[0]
+        ExEy3[x] =integrate.quad(lambda fi:((1-gamma)*cos(alph[x])*cos(fi)*(sin(alph[x])*cos(fi+delta))**3),0,2*pi)[0]
+
+Px=fxxxxx(phi)*Ex4+fxyyyy(phi)*Ey4 +6*fxxxyy(phi)*Ex2Ey2+4*fxxxxy(phi)*Ex3Ey+4*fxxyyy(phi)*ExEy3
+Py=fyyyyy(phi)*Ey4+fyxxxx(phi)*Ex4+6*fyxxyy(phi)*Ex2Ey2+4*fyxyyy(phi)*ExEy3+4*fyxxxy(phi)*Ex3Ey
+
+
+plt.figure()
+plt.polar(alph,Px,'-r',alph,Py,'-b')
+
+
